afdb63e7e6637948523186f26a8354e180ccf852
chaosannals <chaosannals@outlook.com>
2023-10-05 10:10:12 +0800

    save

--- src/pages/github/GhPagesPage.vue
+++ src/pages/github/GhPagesPage.vue
@@ -4,7 +4,9 @@
         <code>
             git subtree push --prefix dist origin gh-pages
         </code>
-        <p>Settings -> GitHub Pages </p>
+        <p>Settings -> Pages 下配置使用 gh-pages 分支。</p>
+        <p>此种方式有个缺陷：dist 作为生成物，必须被加入版本管理。</p>
+        <p>新版 GitHub Pages 支持选中 /(root)  /docs  两个路径，所以完全可以不用上面这种方式，而是在这里选主分支，然后把生成的路径设置成 /docs 就可以</p>
     </div>
 </template>
 
