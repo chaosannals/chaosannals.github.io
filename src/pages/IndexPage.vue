e56b35a01e101f62cc99cbcaf1b639a9521f37f2
chaosannals <chaosannals@outlook.com>
2023-10-05 09:47:58 +0800

    good luck.

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="index-page">
+        <div v-for="link in links" @click="onClickLink(link)">
+            <span>{{ link }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { routes } from '../router';
+import { useRouter } from 'vue-router';
+
+const $router = useRouter();
+
+const links = computed(() => {
+    return routes.map(i => i.path);
+});
+
+const onClickLink = (link: string) => $router.push(link);
+
+</script>
---
+++ src/pages/github/GhPagesPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="github-pages-page">
+        <p>利用 git subtree 可以指定目录推送分支的功能：</p>
+        <code>
+            git subtree push --prefix dist origin gh-pages
+        </code>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
