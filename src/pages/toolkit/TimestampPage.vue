6a6f08c3c27387402f324976f345a97f8cd0784d
chaosannals <chaosannals@outlook.com>
2023-10-05 16:38:13 +0800

    SAVE

---
+++ src/pages/toolkit/TimestampPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="toolkit-timestamp-page">
+        <div>
+            <label>时间戳：</label>
+            <input v-model="data.timestampText" />
+        </div>
+        <div>
+            <label>格式：</label>
+            <input v-model="data.timestampFormat" />
+        </div>
+        <div>
+            <label>结果：</label>
+            <span>{{ timestampResult }}</span>
+        </div>
+        <div>
+            <label>日期：</label>
+            <input v-model="data.dateText" />
+        </div>
+        <div>
+            <label>结果：</label>
+            <span>{{ dateResult }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
+import { format } from 'date-fns';
+
+
+const data = reactive({
+    timestampText: new Date().getTime().toString(),
+    timestampFormat: 'yyyy-MM-dd HH:mm:ss.SSS',
+    dateText: new Date().toUTCString(),
+});
+
+const timestampResult = computed(() => {
+    if (/^\d{10}$/.test(data.timestampText)) {
+        const date = new Date();
+        date.setTime(Number(`${data.timestampText}000`));
+        return format(date, data.timestampFormat);
+    }
+    if (/^\d{13}$/.test(data.timestampText)) {
+        const date = new Date();
+        date.setTime(Number(data.timestampText));
+        return format(date, data.timestampFormat);
+    }
+    return "不是有效的时间戳";
+});
+
+const dateResult = computed(() => {
+    return Date.parse(data.dateText).toString();
+});
+
+</script>
