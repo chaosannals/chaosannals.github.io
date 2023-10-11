f64e3cf45531458e4cc605da2538acbc67f8abfb
chaosannals <chaosannals@outlook.com>
2023-10-11 18:00:56 +0800

    save

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,30 +6,68 @@
             <span>{{ link }}</span>
         </div>
         <h4>博文</h4>
-        <router-link v-for="blog in blogs" :to="blog.url" class="blog-link">
-            <span>{{ blog.title }}</span>
-            <span class="modify-time">{{ blog.mtime }}</span>
-        </router-link>
+        <div v-for="blog in data.blogs">
+            <router-link :to="blog.url" class="blog-link">
+                <span>{{ blog.title }}</span>
+                <span class="modify-time">{{ blog.mtime }}</span>
+            </router-link>
+            <div class="blog-summary" v-html="blog.summary"></div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onBeforeMount, reactive } from 'vue';
 import { routes } from '../router';
 import { useRouter } from 'vue-router';
 import { format } from 'date-fns';
+import { fetchHeadUtf8 } from '../utils/io';
+import WordArray from 'crypto-js/lib-typedarrays';
+import Utf8 from 'crypto-js/enc-utf8';
+import hljs from 'highlight.js';
+import { Marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
+
+const marked = new Marked(
+    markedHighlight({
+        langPrefix: 'hljs language-',
+        highlight(code, lang) {
+            const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+            return hljs.highlight(code, { language }).value;
+        }
+    })
+);
 
 const $router = useRouter();
+
+const data = reactive({
+    blogs: [],
+} as {
+    blogs: BlogMarkdownInfo[],
+});
 
 const links = computed(() => {
     return routes.map(i => i.path);
 });
 
-const blogs = __MARKDOWN_FILES__.map(i => {
-    i.url = `/blog?path=${i.path}`;
-    i.mtime = format(i.modifyAt, 'yyyy-MM-dd HH:ii:ss');
-    return i;
-}).sort((a, b) => a.modifyAt - b.modifyAt);
+const loadSummary = async (path: string): Promise<string> => {
+    const bytes = await fetchHeadUtf8(path, 100);
+    //@ts-ignore
+    const words = WordArray.create(bytes.buffer);
+    const head = Utf8.stringify(words);
+    return await marked.parse(head);;
+}
+
+onBeforeMount(async () => {
+    const tasks = __MARKDOWN_FILES__.map(async i => {
+        i.url = `/blog?path=${i.path}`;
+        i.mtime = format(i.modifyAt, 'yyyy-MM-dd HH:ii:ss');
+        i.summary = await loadSummary(i.path);
+        return i;
+    });
+    const blogs = await Promise.all(tasks);
+    data.blogs = blogs.sort((a, b) => a.modifyAt - b.modifyAt);
+});
 
 const onClickLink = (link: string) => $router.push(link);
 
@@ -64,6 +102,10 @@ const onClickLink = (link: string) => $router.push(link);
             font-size: .8vw;
             color: gray;
         }
+    }
+
+    .blog-summary {
+        background-color: #f4f4f4;
     }
 }
 </style>
--- src/utils/io.ts
+++ src/utils/io.ts
@@ -6,3 +6,36 @@ export async function readAsArrayBuffer(file: Blob) {
         fileReader.readAsArrayBuffer(file);
     });
 }
+
+export async function fetchHeadUtf8(url: string, length: number): Promise<Uint8Array> {
+    const response = await fetch(url);
+    const reader = response.body?.getReader();
+    const buffer = new Uint8Array(length);
+    let index = 0;
+    while (reader != null && index < length) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        for (let i = 0; (i < value.length) && (index < length); ++i) {
+            buffer[index++] = value[i];
+        }
+    }
+    await reader?.cancel();
+
+    // 最后一步 index++ 可能会使得 index == length
+    if (index >= length) {
+        index = length - 1;
+    }
+
+    // UTF8 截取后末尾可能是不完整字符
+    let c = buffer[index];
+    // 排除以 10xxxxxx 后半字符
+    while ((c & 0b11000000) == 0b10000000 && (index > 0)) {
+        c = buffer[--index];
+    }
+    // 排除前半字符 1+0*
+    if (c >= 0b11000000 && (index > 0)) {
+        --index;
+    }
+    // 需要再 new 一个 Uint8Array ，不然共用一个 ArrayBuffer, 而 ArrayBuffer 还是原先的长度，外面要取的是 ArrayBuffer。
+    return (index + 1) < length ? new Uint8Array(buffer.subarray(0, index + 1)) : buffer;
+}
