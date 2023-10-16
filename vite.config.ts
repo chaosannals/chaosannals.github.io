cb1d51232e8da8866575b949546c3c308d2e97a0
chaosannals <chaosannals@outlook.com>
2023-10-16 15:16:36 +0800

    save

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import { dirname, join, relative } from 'path';
 import { openSync, readSync, readdirSync, statSync } from 'fs';
 // node 相关的模块 url path fs 因为 TS 没有申明，所以需要 @types/node 模块
+import { simpleGit, SimpleGit, SimpleGitOptions } from 'simple-git';
 import _ from 'lodash';
 
 const here = fileURLToPath(import.meta.url);
@@ -35,28 +36,56 @@ function readTitle(path: string) {
   return _.trim(title, ' #\r\n');
 }
 
+const gitOptions: Partial<SimpleGitOptions> = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 4,
+  trimmed: false,
+};
+console.log('git baseDir', process.cwd());
+const git: SimpleGit = simpleGit(gitOptions);
+
+async function gitLogTo(path: string) {
+  return new Promise((resolve, reject) => {
+    git.raw(['log', '--reverse', '--format=%ci', '--', path], (e, r) => {
+      if (e) {
+        reject(e);
+      } else {
+        resolve(r.split('\n').filter(i => !_.isEmpty(i)));
+      }
+    });
+  });
+}
+
 // @ts-ignore
-function globBlogMarkdownInfos(dirPath: string): BlogMarkdownInfo[] {
+async function globBlogMarkdownInfos(dirPath: string): BlogMarkdownInfo[] {
   const files = globDir(dirPath, '.md');
-  const paths = files.map(p => {
+  const tasks = files.map(async p => {
     const path = relative(dirPath, p).replace('\\', '/');
     const title = readTitle(p);
     const stat = statSync(p);
+
+    //@ts-ignore
+    const gitLog = await gitLogTo(p);
     return {
       path: `/${path}`,
       title: title,
       createAt: stat.birthtime.getTime(),
       modifyAt: stat.mtime.getTime(),
+      gitLog: gitLog,
     };
   });
-  console.log('globBlogMarkdownInfos', paths);
-  return paths;
+  const result = await Promise.all(tasks);
+  console.log('globBlogMarkdownInfos', result);
+  return result;
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  define: {
-    __MARKDOWN_FILES__: globBlogMarkdownInfos(publicDir),
-  },
+export default defineConfig(async ({ command, mode }) => {
+  return {
+    plugins: [vue()],
+    define: {
+      __MARKDOWN_FILES__: globBlogMarkdownInfos(publicDir),
+    },
+  };
 });
