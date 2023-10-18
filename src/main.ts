f52a82a2c6a4830f3837e06dd3e5919531dfccdd
chaosannals <chaosannals@outlook.com>
2023-10-18 18:02:53 +0800

    save

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import './style.css';
 import 'highlight.js/styles/github.css';
+import 'normalize.css';
 import App from './App.vue';
 import router from './router';
 import loader from './loader';
