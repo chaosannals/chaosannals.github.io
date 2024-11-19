7dadec4c1fd46f41ad5697620614cf29e680cbab
chaosannals <chaosannals@outlook.com>
2024-11-19 17:52:53 +0800

    compile.

--- dist/assets/IndexPage-dbe4f4c7.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,b as c,c as t,e as n}from"./index-e227146f.js";const o={},s=n("h1",null,"Md",-1),_=[s];function a(r,d){return c(),t("div",null,_)}const f=e(o,[["render",a]]);export{f as default};
